f69e6b3e4a674c4a198abee661fca857028dc799
Stuepp <arthuh.c@hotmail.com>
2022-05-07 15:45:16 -0300

    Q28

--- seidal.c
+++ seidal.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-//corrigir!!!
-#define ROWS 3
+//corrigir!!! está ok!
+#define ROWS 4
 
 void jacobi(double A[ROWS][ROWS], double B[ROWS], double chute[ROWS], int n){
     for(int k = 0; k < n; k++){
@@ -20,11 +20,16 @@ void jacobi(double A[ROWS][ROWS], double B[ROWS], double chute[ROWS], int n){
 }
 
 int main(){
-    double A[ROWS][ROWS] = {{-4.22,1.66,-0.96},{-2.24,4.62,0.78},{-3.52,-2.36,-7.48}};
-    double B[ROWS][ROWS] = {2.03,2.36,4.92};
+    double A[ROWS][ROWS] = {
+        {10.68,2.24,-2.13,-4.6},
+        {0.7,-6.66,0.27,3.98},
+        {-0.21,2.65,-5.76,1.19},
+        {3.81,-2.26,-3.85,11.64}
+    };
+    double B[ROWS][ROWS] = {-3.63,-3.99,-3.84,4.36};
 
-    double chute[ROWS] = {-3.63,1.88,-2.23};
-    int n = 18;
+    double chute[ROWS] = {3.44,-2.32,3.35,-4.49};
+    int n = 16;
 
     jacobi(A,B,chute, n);
 }
