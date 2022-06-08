ceda314bad7f922c5831ef86c54beb0229909923
Stuepp <arthuh.c@hotmail.com>
2022-06-08 15:12:58 -0300

    romber

--- T2/best_exp.py
+++ T2/best_exp.py
@@ -21,7 +21,7 @@ def best_poly(x, y, grau = 1):
     return np.linalg.solve(A, B)
 
 def poly(x, a, b):
-    return a*(x / (x+b))
+    return a*x*math.e**(b*x)
 
 def build_func(a, b):
     def temp(x):
@@ -32,8 +32,8 @@ if __name__ == '__main__':
 
     x = [0.5275, 1.2798, 1.8354, 2.995, 3.5563, 4.8647, 5.7268, 6.186, 6.9649, 7.8094, 8.3545, 9.779]
     y = [1.5421, 2.7825, 3.1861, 3.3406, 3.1962, 2.6972, 2.2813, 2.0573, 1.7578, 1.4752, 1.2498, 0.8957]
-    x_ = np.divide(1,x)
-    y_ = np.divide(1,y) # pode mudar com a func
+    x_ = x # pode mudar com a func
+    y_ = np.log(np.divide(y,x)) # pode mudar com a func
 
     grau = 1 # pode mudar com a func
 
@@ -41,8 +41,8 @@ if __name__ == '__main__':
 
     print(f'{a0 = } e {a1 = }')
 
-    a = 1/a0 # pode mudar com a func
-    b = a1*a # pode mudar com a func
+    a = math.e**a1 # pode mudar com a func
+    b = a0 # pode mudar com a func
 
     print(f'{a = } e {b = }')
 
---
+++ T2/romberg.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+
+// 4 poisa primeira coluna deve ter 'error_order/2' elementos
+#define error_order 16
+#define  numElemFristCol 8
+
+// método dos trapezios
+double trapz(double (*f)(double), double a, double b, int n){
+    double soma = 0;
+    double h = (double) (b - a) / (double) n;
+    for(int i = 1; i<n; i++){
+        double xi = a + i * h;
+        soma += f(xi);
+    }
+    soma *= 2;
+    soma += f(a);
+    soma += f(b);
+    soma *= (0.5 * h);
+    return soma;
+}
+
+void romberg(double array[]){
+    // i = 0  está calculando a coluna F2
+    int numCols = error_order / 2.0 - 1;
+    for(int i = 0; i < numCols; i++){
+        for(int j = 0; j < numCols; j++){
+            double numer = pow(2, (i + 1) * 2) * array[j + 1] - array[j];
+            double denom = pow(2, (i + 1) * 2) - 1;
+            array[j] = numer / denom;
+        }
+    }
+    printf("\nAprox O(h^%d)=%.16f", error_order, array[0]);
+}
+
+    // exemplo
+    // aroximar a integral exp(-x*x), de 0 a 1
+double f(double x){
+        return exp(-x*x);
+}
+
+int main(){
+    double a = 0;
+    double b = 1;
+    double h = 0.5;
+    int n = (b - a) / h;
+    double coluna_F1[numElemFristCol] = {};
+    for(int i = 0; i < numElemFristCol; i++){
+        coluna_F1[i] = trapz(f, a, b, (i + 1) * n);
+        printf("\n%.16f", coluna_F1[i]);
+    }
+    romberg(coluna_F1);
+
+    return 0;
+}
