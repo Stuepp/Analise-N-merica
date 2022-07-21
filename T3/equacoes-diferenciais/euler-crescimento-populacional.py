59bbd920346eec7feb2768a333c46eddaec5327c
Stuepp <arthurh.c@hotmail.com>
2022-07-21 17:09:01 -0300

    t3

--- T3/equacoes-diferenciais/euler-crescimento-populacional.py
+++ T3/equacoes-diferenciais/euler-crescimento-populacional.py
@@ -5,12 +5,24 @@ def true_euler(f, k, x0, y0, h, n):
         #print(f'x_{i + 1}={x0} e y_{i+1}={y0}')
     print(f'{y0}')
 
+def euler_mid(f, k, x0, y0, h, n):
+    for _ in range(n):
+        m1 = f(x0, y0, k)
+        m2 = f(x0 + h / 2, y0 + (h / 2) * m1, k)
+        y0 = y0 + h * m2
+        x0 += h
+        yield x0, y0
+
 if __name__ == '__main__':
     def f(x, y, k):
-        return k*y + 34494
+        return k*y + 60062
     
-    x0, y0 = 0.0, 1141534 # x0 = t, y0 = individuos
+    x0, y0 = 0.0, 1711835 # x0 = t, y0 = individuos
     h = 0.0625
-    k = 0.0602
+    k = 0.0877
     n = int(1 / h)
-    true_euler(f, k, x0, y0, h, n)
+    #true_euler(f, k, x0, y0, h, n)
+
+    r2 = euler_mid(f, k, x0, y0, h, n)
+    x2, y2 = zip(*r2)
+    print(y2)
