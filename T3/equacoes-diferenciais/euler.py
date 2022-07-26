3626d64c920b43fe3ae585bbdc8677a5a68bd532
Stuepp <arthurh.c@hotmail.com>
2022-07-26 09:55:03 -0300

    t3

--- T3/equacoes-diferenciais/euler.py
+++ T3/equacoes-diferenciais/euler.py
@@ -22,12 +22,21 @@ def heun(f, x0, y0, h, n):
         x0 += h
         yield [x0, y0]
 
+def ralston(f, x0, y0, h, n):
+    for _ in range(n):
+        m1 = f(x0, y0)
+        m2 = f(x0 + 0.75*h, y0 + 0.75*h * m1)
+        y0 += h * (m1 + 2 * m2) / 3
+        x0 += h
+        yield [x0, y0]
+
+
 if __name__ == '__main__':
     def f(x, y):
         return y * (2 - x) + x + 1
     
-    x0, y0 = 0.904, 2.736
-    h = 0.152
+    x0, y0 = 0.483, 1.299
+    h = 0.15
     n = 10
     """
     r1 = true_euler(f, x0, y0, h, n)
@@ -40,11 +49,16 @@ if __name__ == '__main__':
     x2, y2 = zip(*r2)
     print(y2)
     """
+    """
     r3 =  heun(f, x0, y0, h, n)
     #print(list(r3))
     #print(next(r3))
     x3, y3 = zip(*r3)
     print(y3)
+    """
+    r4 =  ralston(f, x0, y0, h, n)
+    x4, y4 = zip(*r4)
+    print(y4)
 
     #plot 
     """
